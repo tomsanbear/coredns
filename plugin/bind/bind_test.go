9106dad4ef30c6f024d71a79daa4ed77b162da7c
Thomas Santerre <thomas@santerre.xyz>
2019-05-02 08:44:07 -0400

    Add support for stating an interface to bind to.

--- plugin/bind/bind_test.go
+++ plugin/bind/bind_test.go
@@ -20,6 +20,8 @@ func TestSetup(t *testing.T) {
 		{`bind 1.2.3.4 ::5`, []string{"1.2.3.4", "::5"}, false},
 		{`bind ::1 1.2.3.4 ::5 127.9.9.0`, []string{"::1", "1.2.3.4", "::5", "127.9.9.0"}, false},
 		{`bind ::1 1.2.3.4 ::5 127.9.9.0 noone`, nil, true},
+		// {`bind lo0`, []string{"127.0.0.1", "::1"}, false},
+		// {`bind 1.2.3.4 lo`, []string{"1.2.3.4", "127.0.0.1", "::1"}, false},
 	} {
 		c := caddy.NewTestController("dns", test.config)
 		err := setup(c)
--- plugin/bind/setup.go
+++ plugin/bind/setup.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-
 	"github.com/mholt/caddy"
 )
 
@@ -14,19 +13,48 @@ func setup(c *caddy.Controller) error {
 	config := dnsserver.GetConfig(c)
 
 	// addresses will be consolidated over all BIND directives available in that BlocServer
-	all := []string{}
+	bindable := []string{}
 	for c.Next() {
-		addrs := c.RemainingArgs()
-		if len(addrs) == 0 {
+		args := c.RemainingArgs()
+
+		if len(args) == 0 {
 			return plugin.Error("bind", fmt.Errorf("at least one address is expected"))
 		}
-		for _, addr := range addrs {
-			if net.ParseIP(addr) == nil {
-				return plugin.Error("bind", fmt.Errorf("not a valid IP address: %s", addr))
+		for _, arg := range args {
+			// check if user specified
+			if iface, _ := net.InterfaceByName(arg); iface != nil {
+				addrs, err := iface.Addrs()
+				if err != nil {
+					return plugin.Error("bind", fmt.Errorf("no addresses found on interface %v", iface.Name))
+				}
+				var validAddrs []string
+				for _, addr := range addrs {
+
+					validAddr := isValidAddress(addr)
+					if validAddr != "" {
+						validAddrs = append(validAddrs, validAddr)
+					}
+				}
+				if len(validAddrs) < 1 {
+					return plugin.Error("bind", fmt.Errorf("addresses on interface %v were found but not valid: %v", iface.Name, addrs))
+				}
+				bindable = append(bindable, validAddrs...)
+				continue
+			} else if net.ParseIP(arg) == nil {
+				return plugin.Error("bind", fmt.Errorf("not a valid IP address: %s", arg))
 			}
+			bindable = append(bindable, arg)
 		}
-		all = append(all, addrs...)
 	}
-	config.ListenHosts = all
+	config.ListenHosts = bindable
 	return nil
+}
+
+func isValidAddress(address net.Addr) string {
+	// Parse out the IP from the address
+	ip, _, err := net.ParseCIDR(address.String())
+	if err != nil {
+		return ""
+	}
+	return ip.String()
 }
